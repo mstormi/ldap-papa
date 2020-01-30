6f9d5fd40677d41e44a6b2e5b6356a267b2d3c26
Tim Storm <tim.storm@gmx.net>
2020-01-30 17:40:43 +0100

    Fälle 0 structurals 2 structurals implemented, missing case sensitivity

--- ldap-test.py
+++ ldap-test.py
@@ -1,6 +1,21 @@
 import ldif
 
-interessant = [b"TSIdevice",b"inetOrgperson"]
+ALL_STRUCTURALS = [b"TSIdevice",b"inetOrgPerson",b"device", b"person"]
+
+DEFAULT_STRUCTURAL = b"DEFAULTOC"
+
+STRUCTURAL_OBJECTCLASS_MAPPING = {
+    (b"device",b"inetOrgPerson") : [ b"TSIdevice",b"dummyPerson" ]
+
+
+
+
+
+
+}
+
+
+
 
 def sharedClasses(entry, classes):
     return compareClasses(entry,classes).count(True)
@@ -8,8 +23,45 @@ def sharedClasses(entry, classes):
 def compareClasses(entry, classes):
     return [(x in entry["objectclass"]) for x in classes]
 
+def splitClasses(entry, classesToInspect):
+    """
+    Nimmt einen Record und gibt ein Tupel (a, b) mit
+        a alle Klassen in classesToInspect
+        b alle Klassen NICHT in classesToInspect
+    """
+    present = []
+    absent = []
+    for x in entry["objectclass"]:
+        if x in classesToInspect: present.append(x)
+        else: absent.append(x)
+    present.sort()
+    absent.sort()
+    return (present, absent)
+
+
+def addMissingStructural(entry):
+    entry["objectclass"].append(DEFAULT_STRUCTURAL)
+
+def reduceMultipleStructural(entry):
+    structurals, nonstructurals = splitClasses(entry, ALL_STRUCTURALS)
+    if tuple(structurals) in STRUCTURAL_OBJECTCLASS_MAPPING:
+        entry["objectclass"] = nonstructurals + STRUCTURAL_OBJECTCLASS_MAPPING[tuple(structurals)]
+    else: pass
+
+
 with open('datensatz','r') as f:
     recordList = ldif.LDIFRecordList(f)
     recordList.parse()
     for (dn, entry) in recordList.all_records:
-        print(sharedClasses(entry, interessant))
+        print(dn, entry)
+
+        if (sharedClasses(entry, ALL_STRUCTURALS) == 0):
+            addMissingStructural(entry)
+        if (sharedClasses(entry, ALL_STRUCTURALS) == 2):
+            reduceMultipleStructural(entry)
+
+        print(dn, entry, "\n")
+
+
+
+
